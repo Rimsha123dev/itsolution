7f6de83d07b330c1e140d2154a2f8ccfb4d96a5c
Rimsha123dev <153190159+Rimsha123dev@users.noreply.github.com>
2024-12-04 02:20:54 +0500

    commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+
+<div>©2024 ITSolutions. All rights reserved.</div>
+      
+    </div>
+  )
+}
+
+export default Footer
